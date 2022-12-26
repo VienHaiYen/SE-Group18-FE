9ba5d27c3b5ff3f54fa2b487a570f0e1beff69cb
lmtri2307 <94401753+lmtri2307@users.noreply.github.com>
2022-12-26 13:39:33 +0700

    change UI Schedule page

--- student-mangement/src/pages/Schedule/index.js
+++ student-mangement/src/pages/Schedule/index.js
@@ -1,25 +1,62 @@
 // check commit
 function Schedule() {
+    let schedule = [
+        ['Chào cờ', 'Toán', 'Toán', 'Anh', 'Lý', 'Thể dục', 'Sử'],
+        ['Công nghệ', 'Công nghệ', 'Thẻ dục', 'Tự học', 'Sinh', 'Văn', 'Văn'],
+        ['Địa', 'Anh', 'Anh', 'GDCD', 'Tin', 'Toán', 'Văn'],
+        ['Hóa', 'Hóa', 'Sử', 'Lý', 'Sinh', 'Anh', 'Văn'],
+        ['Lý', 'Toán', 'Toán', 'Hóa', 'Sinh', 'Văn', null],
+    ];
+
     let isStudent = false;
+    function transposeArray(array, arrayLength) {
+        var newArray = [];
+        for (var i = 0; i < arrayLength; i++) {
+            newArray.push(new Array(array.length));
+        }
+        for (var i = 0; i < newArray.length; i++) {
+            for (var j = 0; j < array.length; j++) {
+                newArray[i][j] = array[j][i];
+            }
+        }
+        console.log(newArray);
+        return newArray;
+    }
     return (
         <>
+            <h2>Thời khóa biểu học sinh</h2>
             <form>
-                <h2>Thời khóa biểu học sinh</h2>
                 <div className="form-row align-items-end">
-                    <div className="form-group mr-3">
-                        <label htmlFor="inputEmail4">Tên lớp</label>
-                        <input type="text" className=" form-control" id="inputEmail4" placeholder="Tên lớp" />
-                    </div>
-                    <div className="form-group mr-3">
-                        <label htmlFor="inputPassword4">Giáo viên chủ nhiệm</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="inputPassword4"
-                            placeholder="--Giáo viên chủ nhiệm--"
-                            disabled
-                        />
-                    </div>
+                    {!isStudent && (
+                        <>
+                            <div className="form-group mr-3">
+                                <label htmlFor="inputEmail4">Tên lớp</label>
+                                <input type="text" className=" form-control" id="inputEmail4" placeholder="Tên lớp" />
+                            </div>
+                            <div className="form-group mr-3">
+                                <label htmlFor="inputPassword4">Giáo viên chủ nhiệm</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="inputPassword4"
+                                    placeholder="--Giáo viên chủ nhiệm--"
+                                    disabled
+                                />
+                            </div>
+                        </>
+                    )}
+                    {isStudent && (
+                        <div className="form-group mr-3">
+                            <label htmlFor="inputEmail4">Tên lớp</label>
+                            <input
+                                type="text"
+                                className=" form-control"
+                                disabled
+                                id="inputEmail4"
+                                placeholder="Tên lớp"
+                            />
+                        </div>
+                    )}
                     <div className="form-group mr-3">
                         <label htmlFor="inputState">Năm học</label>
                         <select id="inputState" className="form-control">
@@ -63,70 +100,30 @@ function Schedule() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr>
-                        <th scope="row"></th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                        <td>@twitter</td>
-                    </tr>
+                    {transposeArray(schedule, 7).map((row, index) =>
+                        index === 4 ? (
+                            <>
+                                <br />
+                                <tr>
+                                    <th scope="col">{index + 1}</th>
+                                    <td>{row[0]}</td>
+                                    <td>{row[1]}</td>
+                                    <td>{row[2]}</td>
+                                    <td>{row[3]}</td>
+                                    <td>{row[4]}</td>
+                                </tr>
+                            </>
+                        ) : (
+                            <tr>
+                                <th scope="col">{index + 1}</th>
+                                <td>{row[0]}</td>
+                                <td>{row[1]}</td>
+                                <td>{row[2]}</td>
+                                <td>{row[3]}</td>
+                                <td>{row[4]}</td>
+                            </tr>
+                        ),
+                    )}
                 </tbody>
             </table>
         </>
