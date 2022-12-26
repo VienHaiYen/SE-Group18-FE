b199eb3c20afcdde9e8ce9e46d47ce53c2fcaec4
lmtri2307 <94401753+lmtri2307@users.noreply.github.com>
2022-12-27 00:09:12 +0700

    kết nối UserLayout với database

--- student-mangement/src/layouts/UserLayout/index.js
+++ student-mangement/src/layouts/UserLayout/index.js
@@ -1,29 +1,29 @@
 import { useState, useEffect } from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-function UserLayout({ children }) {
-    let role = 2;
-    let user = {
-        name: 'Hai Yen',
-        role: 'student',
-    };
+function UserLayout({ children, loginState }) {
     const [navs, setNavs] = useState([]);
+    const [user, setUser] = useState({
+        name: '',
+        role: '',
+    });
     useEffect(() => {
-        setNavInRules();
+        console.log(loginState);
+        setNavs(loginState.path);
+        setUser({
+            name: loginState.name,
+            role: loginState.role,
+        });
     }, []);
 
-    const setNavInRules = async () => {
-        const res = await fetch(`http://localhost:5000/user-layout/${role}`);
-        let data = await res.json();
-        data = data['display'];
-        setNavs(data);
-    };
     return (
         <div className="d-flex ">
             <Header navItems={navs} userInfo={user} />
             <div className="p-5" style={{ width: '100%' }}>
-                {children}
+                {React.cloneElement(children, { id: loginState.id })}
+                {/* {children} */}
             </div>
             <Footer />
         </div>
